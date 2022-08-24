2ee39680e05bdb19ecf7d8876b64f7e7e75d7eca
kaoru <shiina_kaoru@outlook.com>
2022-08-24 14:39:45 +0800

    Initial commit

---
+++ include/SString/SString.h
@@ -0,0 +1,39 @@
+namespace sstr {
+
+    class SString {
+        /// 构造相关
+    public:
+        static SString fromASCII(const char *);
+        static SString fromUTF8(const char *);
+//        static SString fromUTF16(const wchar_t *);
+//        static SString fromUTF32(const wchar_t *);
+
+        // 基础功能
+    public:
+        /// 等价于 emtpy
+        /// \retval true 字符串为空
+        /// \retval false 字符串不为空
+        bool null() const;
+        /// 字符串是否为空
+        /// \retval true 字符串为空
+        /// \retval false 字符串不为空
+        bool emtpy() const;
+        /// 获取字符串中字符个数
+        /// \return 字符个数
+        size_t len() const;
+        /// 获取缓存区容量
+        /// \return 缓冲区容量
+        size_t cap() const;
+        /// 获取缓冲区已用大小
+        /// \return 缓冲区已用大小
+        size_t size() const;
+        /// 获取缓冲区指针
+        /// \return 缓冲区指针
+        const char *data() const;
+
+    private:
+        char *_data = nullptr;
+        size_t _capacity = 0;
+        size_t _size = 0;
+    };
+}// namespace sstr
