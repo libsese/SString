c5ab97ddc73075367100a0d7993864c5efc7f224
Kaoru <SHIINA_KAORU@outlook.com>
2022-10-08 12:38:48 +0800

    简化描述，添加忽略目录

--- xmake.lua
+++ xmake.lua
@@ -1,34 +1,32 @@
+set_project("SString")
+set_version("0.1", {build = "%Y%m%d%H%M"})
+set_allowedplats("windows", "linux", "macosx")
+set_allowedarchs("x64", "x86")
 set_languages("c99", "cxx11")
 add_includedirs("include")
+
+if is_os("windows") then
+	add_cxxflags("/utf-8")
+end
 
 target("SString")
 	set_kind("static")
 	add_files("src/*.cpp")
 
 target("TestSString")
+	set_enabled(false)
 	set_kind("binary")
-	if is_os("windows") then
-		add_cxxflags("/utf-8")
-	end
 	add_deps("SString")
 	add_files("test/TestSString.cpp")
 
 target("TestSStringBuilder")
 	set_enabled(false)
 	set_kind("binary")
-	if is_os("windows") then
-		add_cxxflags("/utf-8")
-	end
-	add_cxxflags("/utf-8")
 	add_deps("SString")
 	add_files("test/TestSStringBuilder.cpp")
 
 target("TestAlgol")
 	set_enabled(false)
 	set_kind("binary")
-	if is_os("windows") then
-		add_cxxflags("/utf-8")
-	end
-	add_cxxflags("/utf-8")
 	add_deps("SString")
 	add_files("test/TestAlgol.cpp")
